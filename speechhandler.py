77036685b362b365f105cf05da7bf565d956a802
thomasdimeny <crow@crows-MacBook-Air.local>
2026-07-20 16:30:01 -0600

    fixed audio issues, updated req.txt

--- speechhandler.py
+++ speechhandler.py
@@ -3,8 +3,8 @@ import speech_recognition as sr
 import pyaudio
 import ollama
 import pyttsx3
-#from  commandhandler import intake_command
-#from commandlibrary import playlist, commandLib
+# from  commandhandler import intake_command
+# from commandlibrary import playlist, commandLib
 
 # initialize recognizer and text to speech initialization
 # recognizer = sr.Recognizer()
@@ -12,19 +12,28 @@ myrspeech = pyttsx3.init()
 
 
 personality = "You are a helpful automaton named Myr, (prounounced meer). You speak in partial sentences with a robotic cadence and lack an understanding of many human sentiments."
-ollama.create(model="myr", from_="phi3", system=personality)
+try:
+    ollama.show("myr")
+except Exception:
+    ollama.create(
+        model="myr",
+        from_="phi3",
+        system=personality,
+    )
+microphone = ""
 for i, name in enumerate(sr.Microphone.list_microphone_names()):
-    print(i, name)
+    if name == "MacBook Air Microphone":
+        microphone = name
 
 
 def audioIntake():
     print("waiting for input:")
     recognizer = sr.Recognizer()
 
-    with sr.Microphone(device_index=12) as microphone:
+    with sr.Microphone() as microphone:
         recognizer.adjust_for_ambient_noise(microphone, duration=1)
         try:
-            audio = recognizer.listen(microphone, timeout=5, phrase_time_limit=10)
+            audio = recognizer.listen(microphone, timeout=None, phrase_time_limit=10)
             return recognizer.recognize_google(audio)
         except sr.WaitTimeoutError:
             print("No input")
@@ -38,18 +47,24 @@ def audioIntake():
             return None
 
 
-while True:
-
+loopVar = True
+while loopVar == True:
     userInput = audioIntake()
 
     if not userInput:
         continue
 
+    if "exit program" in userInput:
+        loopVar = False
+        break
+
     print(userInput)
     response = ollama.chat(
         model="myr", messages=[{"role": "user", "content": userInput}]
     )
-    myrspeech.say(response.message.content)
+    text = response.message.content
+    print(text)
+    myrspeech.stop()
+    myrspeech.say(text)
     myrspeech.runAndWait()
-    # or access fields directly from the response object
-    print(response.message.content)
+    # or access fields directly from the respons
